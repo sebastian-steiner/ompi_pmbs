jsquyres/ompi

Added Software-based Performance Counters driver code along with several counters.
This code is the implementation of Software-base Performance Counters as described in the paper 'Using Software-Base Performance Counters to Expose Low-Level Open MPI Performance Information' in EuroMPI/USA '17 (http://icl.cs.utk.edu/news_pub/submissions/software-performance-counters.pdf).  More practical usage information can be found here: https://github.com/davideberius/ompi/wiki/How-to-Use-Software-Based-Performance-Counters-(SPCs)-in-Open-MPI.

All software events functions are put in macros that become no-ops when SOFTWARE_EVENTS_ENABLE is not defined.  The internal timer units have been changed to cycles to avoid division operations which was a large source of overhead as discussed in the paper.  Added a --with-spc configure option to enable SPCs in the Open MPI build.  This defines SOFTWARE_EVENTS_ENABLE.  Added an MCA parameter, mpi_spc_enable, for turning on specific counters.  Added an MCA parameter, mpi_spc_dump_enabled, for turning on and off dumping SPC counters in MPI_Finalize.  Added an SPC test and example.

Signed-off-by: David Eberius <deberius@vols.utk.edu>

ompi/mpi/c/alltoall.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2017 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -33,6 +33,7 @@
 #include "ompi/errhandler/errhandler.h"
 #include "ompi/datatype/ompi_datatype.h"
 #include "ompi/memchecker.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -50,6 +51,8 @@ int MPI_Alltoall(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
 {
     int err;
     size_t recvtype_size;
+
+    SPC_RECORD(OMPI_ALLTOALL, 1);
 
     MEMCHECKER(
         memchecker_comm(comm);

ompi/mpi/c/bsend.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -31,6 +31,7 @@
 #include "ompi/mca/pml/pml.h"
 #include "ompi/mca/pml/base/pml_base_bsend.h"
 #include "ompi/memchecker.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -45,6 +46,8 @@ static const char FUNC_NAME[] = "MPI_Bsend";
 int MPI_Bsend(const void *buf, int count, MPI_Datatype type, int dest, int tag, MPI_Comm comm)
 {
     int rc = MPI_SUCCESS;
+
+    SPC_RECORD(OMPI_BSEND, 1);
 
     MEMCHECKER(
         memchecker_datatype(type);

ompi/mpi/c/ialltoall.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2017 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -32,6 +32,7 @@
 #include "ompi/errhandler/errhandler.h"
 #include "ompi/datatype/ompi_datatype.h"
 #include "ompi/memchecker.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -49,6 +50,8 @@ int MPI_Ialltoall(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
 {
     size_t sendtype_size, recvtype_size;
     int err;
+
+    SPC_RECORD(OMPI_IALLTOALL, 1);
 
     MEMCHECKER(
         memchecker_comm(comm);

ompi/mpi/c/ibsend.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -31,6 +31,7 @@
 #include "ompi/mca/pml/pml.h"
 #include "ompi/mca/pml/base/pml_base_bsend.h"
 #include "ompi/memchecker.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -46,6 +47,8 @@ int MPI_Ibsend(const void *buf, int count, MPI_Datatype type, int dest,
                int tag, MPI_Comm comm, MPI_Request *request)
 {
     int rc = MPI_SUCCESS;
+
+    SPC_RECORD(OMPI_IBSEND, 1);
 
     MEMCHECKER(
         memchecker_datatype(type);

ompi/mpi/c/iprobe.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -27,6 +27,7 @@
 #include "ompi/mca/pml/pml.h"
 #include "ompi/memchecker.h"
 #include "ompi/request/request.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -41,6 +42,8 @@ static const char FUNC_NAME[] = "MPI_Iprobe";
 int MPI_Iprobe(int source, int tag, MPI_Comm comm, int *flag, MPI_Status *status)
 {
     int rc;
+
+    SPC_RECORD(OMPI_IPROBE, 1);
 
     MEMCHECKER(
         memchecker_comm(comm);

ompi/mpi/c/iscatterv.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2017 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -30,6 +30,7 @@
 #include "ompi/errhandler/errhandler.h"
 #include "ompi/datatype/ompi_datatype.h"
 #include "ompi/memchecker.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -46,6 +47,8 @@ int MPI_Iscatterv(const void *sendbuf, const int sendcounts[], const int displs[
                   MPI_Datatype recvtype, int root, MPI_Comm comm, MPI_Request *request)
 {
     int i, size, err;
+
+    SPC_RECORD(OMPI_ISCATTERV, 1);
 
     MEMCHECKER(
         ptrdiff_t ext;

test/spc/spc_test.c
@@ -1,0 +1,176 @@
+/*
+ * Copyright (c) 2018 The University of Tennessee and The University
+ *                    of Tennessee Research Foundation.  All rights
+ *                    reserved.
+ *
+ * Simple example usage of SPCs through MPI_T.
+ */
+
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 1000000
+
+/* Sends 'num_messages' messages of 'message_size' bytes from rank 0 to rank 1.
+ * All messages are sent synchronously and with the same tag in MPI_COMM_WORLD.
+ */
+void message_exchange(int num_messages, int message_size)
+{
+    int i, rank;
+    /* Use calloc to initialize data to 0's */
+    char *data = (char*)calloc(message_size, sizeof(char));
+    MPI_Status status;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if(rank == 0) {
+        for(i = 0; i < num_messages; i++)
+            MPI_Send(data, message_size, MPI_BYTE, 1, 123, MPI_COMM_WORLD);
+    } else if(rank == 1) {
+        for(i = 0; i < num_messages; i++)
+            MPI_Recv(data, message_size, MPI_BYTE, 0, 123, MPI_COMM_WORLD, &status);
+    }
+
+    free(data);
+}
+
+int main(int argc, char **argv)
+{
+    int i, rank, size, provided, num, name_len, desc_len, verbosity, bind, var_class, readonly, continuous, atomic, count, index, MPI_result;
+    MPI_Datatype datatype;
+    MPI_T_enum enumtype;
+    MPI_Comm comm;
+    char name[256], description[256];
+
+    /* Counter names to be read by ranks 0 and 1 */
+    char counter_names[2][40];
+    sprintf(counter_names[0], "runtime_spc_OMPI_BYTES_SENT_USER");
+    sprintf(counter_names[1], "runtime_spc_OMPI_BYTES_RECEIVED_USER");
+
+    MPI_Init(NULL, NULL);
+    MPI_result = MPI_T_init_thread(MPI_THREAD_SINGLE, &provided);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to initialize MPI_T thread.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if(size != 2) {
+        fprintf(stderr, "ERROR: This test should be run with two MPI processes.\n");
+        return -1;
+    }
+
+    /* Determine the MPI_T pvar indices for the OMPI_BYTES_SENT/RECIEVED_USER SPCs */
+    index = -1;
+    MPI_result = MPI_T_pvar_get_num(&num);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to get the number of pvars.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    for(i = 0; i < num; i++) {
+        name_len = desc_len = 256;
+        MPI_T_pvar_get_info(i, name, &name_len, &verbosity,
+                             &var_class, &datatype, &enumtype, description, &desc_len, &bind,
+                             &readonly, &continuous, &atomic);
+        if(MPI_result != MPI_SUCCESS || MPI_result == MPI_T_ERR_PVAR_NO_STARTSTOP) {
+            fprintf(stderr, "Failed to get pvar info.\n");
+            MPI_Abort(MPI_COMM_WORLD, MPI_result);
+        }
+
+        if(strcmp(name, counter_names[rank]) == 0) {
+            index = i;
+            printf("[%d] %s -> %s\n", rank, name, description);
+        }
+    }
+
+    /* Make sure we found the counters */
+    if(index == -1) {
+        fprintf(stderr, "ERROR: Couldn't find the appropriate SPC counter in the MPI_T pvars.\n");
+        return -1;
+    }
+
+    long long value;
+
+    MPI_T_pvar_session session;
+    MPI_T_pvar_handle handle;
+    /* Create the MPI_T sessions/handles for the counters and start the counters */
+    MPI_result = MPI_T_pvar_session_create(&session);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to create MPI_T pvar session.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_result = MPI_T_pvar_handle_alloc(session, index, NULL, &handle, &count);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to allocate the pvar handle.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_result = MPI_T_pvar_start(session, handle);
+    if(MPI_result != MPI_SUCCESS) {
+        if(MPI_result != MPI_T_ERR_PVAR_NO_STARTSTOP) {
+            fprintf(stderr, "Failed to start the pvar session.\n");
+            MPI_Abort(MPI_COMM_WORLD, MPI_result);
+        }
+    }
+
+    int message_size = 1, expected_bytes = 0;
+    while(message_size <= MAX_SIZE) {
+        expected_bytes += message_size;
+        message_exchange(1, message_size);
+        message_size *= 10;
+    }
+
+    MPI_result = MPI_T_pvar_read(session, handle, &value);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to read the pvar.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    /* Print the counter values in order by rank */
+    for(i = 0; i < 2; i++) {
+        if(i == rank) {
+            printf("[%d] Value Read: %lld\n", rank, value);
+            fflush(stdout);
+            if(value != expected_bytes){
+                fprintf(stderr, "The counter value is inaccurate!  It is '%d'.  It should be '%d'\n", value, expected_bytes);
+                MPI_Abort(MPI_COMM_WORLD, MPI_ERR_OTHER);
+            }
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+    /* Stop the MPI_T session, free the handle, and then free the session */
+    MPI_result = MPI_T_pvar_stop(session, handle);
+    if(MPI_result != MPI_SUCCESS) {
+        if(MPI_result != MPI_T_ERR_PVAR_NO_STARTSTOP) {
+            fprintf(stderr, "Failed to stop the pvar session.\n");
+            MPI_Abort(MPI_COMM_WORLD, MPI_result);
+        }
+    }
+
+    MPI_result = MPI_T_pvar_handle_free(session, &handle);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to free the pvar handle.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_result = MPI_T_pvar_session_free(&session);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to free the pvar session.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_result = MPI_T_finalize();
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to finalize MPI_T.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_Finalize();
+
+    return EXIT_SUCCESS;
+}

sebastian-steiner/ompi_pmbs

ompi/mpi/c/alltoall.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2017 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -33,6 +33,7 @@
 #include "ompi/errhandler/errhandler.h"
 #include "ompi/datatype/ompi_datatype.h"
 #include "ompi/memchecker.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -50,6 +51,8 @@ int MPI_Alltoall(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
 {
     int err;
     size_t recvtype_size;
+
+    SPC_RECORD(OMPI_SPC_ALLTOALL, 1);
 
     MEMCHECKER(
         memchecker_comm(comm);

ompi/mpi/c/bsend.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -31,6 +31,7 @@
 #include "ompi/mca/pml/pml.h"
 #include "ompi/mca/pml/base/pml_base_bsend.h"
 #include "ompi/memchecker.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -45,6 +46,8 @@ static const char FUNC_NAME[] = "MPI_Bsend";
 int MPI_Bsend(const void *buf, int count, MPI_Datatype type, int dest, int tag, MPI_Comm comm)
 {
     int rc = MPI_SUCCESS;
+
+    SPC_RECORD(OMPI_SPC_BSEND, 1);
 
     MEMCHECKER(
         memchecker_datatype(type);

ompi/mpi/c/ialltoall.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2017 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -32,6 +32,7 @@
 #include "ompi/errhandler/errhandler.h"
 #include "ompi/datatype/ompi_datatype.h"
 #include "ompi/memchecker.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -49,6 +50,8 @@ int MPI_Ialltoall(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
 {
     size_t sendtype_size, recvtype_size;
     int err;
+
+    SPC_RECORD(OMPI_SPC_IALLTOALL, 1);
 
     MEMCHECKER(
         memchecker_comm(comm);

ompi/mpi/c/ibsend.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -31,6 +31,7 @@
 #include "ompi/mca/pml/pml.h"
 #include "ompi/mca/pml/base/pml_base_bsend.h"
 #include "ompi/memchecker.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -46,6 +47,8 @@ int MPI_Ibsend(const void *buf, int count, MPI_Datatype type, int dest,
                int tag, MPI_Comm comm, MPI_Request *request)
 {
     int rc = MPI_SUCCESS;
+
+    SPC_RECORD(OMPI_SPC_IBSEND, 1);
 
     MEMCHECKER(
         memchecker_datatype(type);

ompi/mpi/c/iprobe.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -27,6 +27,7 @@
 #include "ompi/mca/pml/pml.h"
 #include "ompi/memchecker.h"
 #include "ompi/request/request.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -41,6 +42,8 @@ static const char FUNC_NAME[] = "MPI_Iprobe";
 int MPI_Iprobe(int source, int tag, MPI_Comm comm, int *flag, MPI_Status *status)
 {
     int rc;
+
+    SPC_RECORD(OMPI_SPC_IPROBE, 1);
 
     MEMCHECKER(
         memchecker_comm(comm);

ompi/mpi/c/iscatterv.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2017 The University of Tennessee and The University
+ * Copyright (c) 2004-2018 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2008 High Performance Computing Center Stuttgart,
@@ -30,6 +30,7 @@
 #include "ompi/errhandler/errhandler.h"
 #include "ompi/datatype/ompi_datatype.h"
 #include "ompi/memchecker.h"
+#include "ompi/runtime/ompi_spc.h"
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -46,6 +47,8 @@ int MPI_Iscatterv(const void *sendbuf, const int sendcounts[], const int displs[
                   MPI_Datatype recvtype, int root, MPI_Comm comm, MPI_Request *request)
 {
     int i, size, err;
+
+    SPC_RECORD(OMPI_SPC_ISCATTERV, 1);
 
     MEMCHECKER(
         ptrdiff_t ext;

test/spc/spc_test.c
@@ -1,0 +1,174 @@
+/*
+ * Copyright (c) 2018 The University of Tennessee and The University
+ *                    of Tennessee Research Foundation.  All rights
+ *                    reserved.
+ *
+ * Simple example usage of SPCs through MPI_T.
+ */
+
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 1000000
+
+/* Sends 'num_messages' messages of 'message_size' bytes from rank 0 to rank 1.
+ * All messages are sent synchronously and with the same tag in MPI_COMM_WORLD.
+ */
+static void message_exchange(int num_messages, int message_size)
+{
+    int i, rank;
+    /* Use calloc to initialize data to 0's */
+    char *data = (char*)calloc(message_size, sizeof(char));
+    MPI_Status status;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if(rank == 0) {
+        for(i = 0; i < num_messages; i++)
+            MPI_Send(data, message_size, MPI_BYTE, 1, 123, MPI_COMM_WORLD);
+    } else if(rank == 1) {
+        for(i = 0; i < num_messages; i++)
+            MPI_Recv(data, message_size, MPI_BYTE, 0, 123, MPI_COMM_WORLD, &status);
+    }
+
+    free(data);
+}
+
+int main(int argc, char **argv)
+{
+    int i, rank, size, provided, num, name_len, desc_len, verbosity, bind, var_class, readonly, continuous, atomic, count, index, MPI_result;
+    MPI_Datatype datatype;
+    MPI_T_enum enumtype;
+    char name[256], description[256];
+
+    /* Counter names to be read by ranks 0 and 1 */
+    char *counter_names[] = { "runtime_spc_OMPI_BYTES_SENT_USER",
+                              "runtime_spc_OMPI_BYTES_RECEIVED_USER" };
+
+    MPI_Init(NULL, NULL);
+    MPI_result = MPI_T_init_thread(MPI_THREAD_SINGLE, &provided);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to initialize MPI_T thread.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if(size != 2) {
+        fprintf(stderr, "ERROR: This test should be run with two MPI processes.\n");
+        MPI_Abort(MPI_COMM_WORLD, -1);
+    }
+
+    /* Determine the MPI_T pvar indices for the OMPI_BYTES_SENT/RECIEVED_USER SPCs */
+    index = -1;
+    MPI_result = MPI_T_pvar_get_num(&num);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to get the number of pvars.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    for(i = 0; i < num; i++) {
+        name_len = desc_len = 256;
+        MPI_T_pvar_get_info(i, name, &name_len, &verbosity,
+                             &var_class, &datatype, &enumtype, description, &desc_len, &bind,
+                             &readonly, &continuous, &atomic);
+        if(MPI_result != MPI_SUCCESS || MPI_result == MPI_T_ERR_PVAR_NO_STARTSTOP) {
+            fprintf(stderr, "Failed to get pvar info.\n");
+            MPI_Abort(MPI_COMM_WORLD, MPI_result);
+        }
+
+        if(strcmp(name, counter_names[rank]) == 0) {
+            index = i;
+            printf("[%d] %s -> %s\n", rank, name, description);
+        }
+    }
+
+    /* Make sure we found the counters */
+    if(index == -1) {
+        fprintf(stderr, "ERROR: Couldn't find the appropriate SPC counter in the MPI_T pvars.\n");
+        MPI_Abort(MPI_COMM_WORLD, -1);
+    }
+
+    long long value;
+
+    MPI_T_pvar_session session;
+    MPI_T_pvar_handle handle;
+    /* Create the MPI_T sessions/handles for the counters and start the counters */
+    MPI_result = MPI_T_pvar_session_create(&session);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to create MPI_T pvar session.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_result = MPI_T_pvar_handle_alloc(session, index, NULL, &handle, &count);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to allocate the pvar handle.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_result = MPI_T_pvar_start(session, handle);
+    if(MPI_result != MPI_SUCCESS) {
+        if(MPI_result != MPI_T_ERR_PVAR_NO_STARTSTOP) {
+            fprintf(stderr, "Failed to start the pvar session.\n");
+            MPI_Abort(MPI_COMM_WORLD, MPI_result);
+        }
+    }
+
+    int message_size = 1, expected_bytes = 0;
+    while(message_size <= MAX_SIZE) {
+        expected_bytes += message_size;
+        message_exchange(1, message_size);
+        message_size *= 10;
+    }
+
+    MPI_result = MPI_T_pvar_read(session, handle, &value);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to read the pvar.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    /* Print the counter values in order by rank */
+    for(i = 0; i < 2; i++) {
+        if(i == rank) {
+            printf("[%d] Value Read: %lld\n", rank, value);
+            fflush(stdout);
+            if(value != expected_bytes){
+                fprintf(stderr, "The counter value is inaccurate!  It is '%lld'.  It should be '%d'\n", value, expected_bytes);
+                MPI_Abort(MPI_COMM_WORLD, MPI_ERR_OTHER);
+            }
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+    /* Stop the MPI_T session, free the handle, and then free the session */
+    MPI_result = MPI_T_pvar_stop(session, handle);
+    if(MPI_result != MPI_SUCCESS) {
+        if(MPI_result != MPI_T_ERR_PVAR_NO_STARTSTOP) {
+            fprintf(stderr, "Failed to stop the pvar session.\n");
+            MPI_Abort(MPI_COMM_WORLD, MPI_result);
+        }
+    }
+
+    MPI_result = MPI_T_pvar_handle_free(session, &handle);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to free the pvar handle.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_result = MPI_T_pvar_session_free(&session);
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to free the pvar session.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_result = MPI_T_finalize();
+    if(MPI_result != MPI_SUCCESS) {
+        fprintf(stderr, "Failed to finalize MPI_T.\n");
+        MPI_Abort(MPI_COMM_WORLD, MPI_result);
+    }
+
+    MPI_Finalize();
+
+    return EXIT_SUCCESS;
+}