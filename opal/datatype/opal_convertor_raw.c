uiuc-hpc/parsec

opal/datatype: fix opal_convertor_raw
correctly handle the case in which iovec is full and the
last accessed element of the datatype is the beginning of a loop

Refs. open-mpi/ompi#6285

Thanks Axel Huebl for reporting this

Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

parsec/datatype/opal_convertor_raw.c
@@ -5,7 +5,7 @@
  *                         reserved.
  * Copyright (c) 2009      Oak Ridge National Labs.  All rights reserved.
  * Copyright (c) 2013      Cisco Systems, Inc.  All rights reserved.
- * Copyright (c) 2017-2018 Research Organization for Information Science
+ * Copyright (c) 2017-2019 Research Organization for Information Science
  *                         and Technology (RIST).  All rights reserved.
  * $COPYRIGHT$
  *
@@ -170,8 +170,18 @@ opal_convertor_raw( opal_convertor_t* pConvertor,
             ddt_endloop_desc_t* end_loop = (ddt_endloop_desc_t*)(pElem + pElem->loop.items);
 
             if( pElem->loop.common.flags & OPAL_DATATYPE_FLAG_CONTIGUOUS ) {
-                source_base += end_loop->first_elem_disp;
-                for(size_t i = count_desc; (i > 0) && (index < *iov_count); i--, index++ ) {
+                ptrdiff_t offset = end_loop->first_elem_disp;
+                source_base += offset;
+                for(size_t i = count_desc; i > 0; i--, index++ ) {
+                    if (index >= *iov_count) {
+                        dt_elem_desc_t* nElem = pElem + 1;
+                        while (nElem->elem.common.type == OPAL_DATATYPE_LOOP) {
+                            nElem++;
+                        }
+                        assert(OPAL_DATATYPE_END_LOOP != nElem->elem.common.type);
+                        offset = nElem->elem.disp;
+                        break;
+                    }
                     OPAL_DATATYPE_SAFEGUARD_POINTER( source_base, end_loop->size, pConvertor->pBaseBuf,
                                                 pConvertor->pDesc, pConvertor->count );
                     iov[index].iov_base = (IOVBASE_TYPE *) source_base;
@@ -180,7 +190,7 @@ opal_convertor_raw( opal_convertor_t* pConvertor,
                     raw_data += end_loop->size;
                     count_desc--;
                 }
-                source_base -= end_loop->first_elem_disp;
+                source_base -= offset;
                 if( 0 == count_desc ) {  /* completed */
                     pos_desc += pElem->loop.items + 1;
                     goto update_loop_description;

sebastian-steiner/ompi_pmbs

(back-ported from commit open-mpi/ompi@0832ab5acc2419e670b44ff3857c8c3d21c78e09)

opal/datatype/opal_convertor_raw.c
@@ -5,8 +5,8 @@
  *                         reserved.
  * Copyright (c) 2009      Oak Ridge National Labs.  All rights reserved.
  * Copyright (c) 2013      Cisco Systems, Inc.  All rights reserved.
- * Copyright (c) 2017      Research Organization for Information Science
- *                         and Technology (RIST). All rights reserved.
+ * Copyright (c) 2017-2019 Research Organization for Information Science
+ *                         and Technology (RIST).  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -170,9 +170,18 @@ opal_convertor_raw( opal_convertor_t* pConvertor,
             ddt_endloop_desc_t* end_loop = (ddt_endloop_desc_t*)(pElem + pElem->loop.items);
 
             if( pElem->loop.common.flags & OPAL_DATATYPE_FLAG_CONTIGUOUS ) {
-                uint32_t i;
-                source_base += end_loop->first_elem_disp;
-                for( i = count_desc; (i > 0) && (index < *iov_count); i--, index++ ) {
+                ptrdiff_t offset = end_loop->first_elem_disp;
+                source_base += offset;
+                for(uint32_t i = count_desc; i > 0; i--, index++ ) {
+                    if (index >= *iov_count) {
+                        dt_elem_desc_t* nElem = pElem + 1;
+                        while (nElem->elem.common.type == OPAL_DATATYPE_LOOP) {
+                            nElem++;
+                        }
+                        assert(OPAL_DATATYPE_END_LOOP != nElem->elem.common.type);
+                        offset = nElem->elem.disp;
+                        break;
+                    }
                     OPAL_DATATYPE_SAFEGUARD_POINTER( source_base, end_loop->size, pConvertor->pBaseBuf,
                                                 pConvertor->pDesc, pConvertor->count );
                     iov[index].iov_base = (IOVBASE_TYPE *) source_base;
@@ -181,7 +190,7 @@ opal_convertor_raw( opal_convertor_t* pConvertor,
                     raw_data += end_loop->size;
                     count_desc--;
                 }
-                source_base -= end_loop->first_elem_disp;
+                source_base -= offset;
                 if( 0 == count_desc ) {  /* completed */
                     pos_desc += pElem->loop.items + 1;
                     goto update_loop_description;