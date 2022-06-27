cb8ad068925b4e79db8295acd70e2c3b1f9b0c92
Sascha Hunold <sascha@hunoldscience.net>
2022-06-27 17:23:24 +0200

    added tuner dummy file

---
+++ ompi/runtime/at_coll_tuner_dummy.c
@@ -0,0 +1,13 @@
+//
+// Created by Sascha on 6/27/22.
+//
+
+#include "at_coll_tuner.h"
+
+void AT_coll_tune_init(void) {
+  printf("dummy\n");
+}
+
+void AT_coll_tune_finalize(void) {
+  printf("dummy\n");
+}
