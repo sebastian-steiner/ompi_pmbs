2a54dd2002e3e53a1eeca1cccc7ff907b820b68a
Sebastian Steiner <sebastian.steiner@tuta.io>
2022-07-08 20:51:33 +0200

    Add stubs for other collectives

---
+++ ompi/runtime/at_coll_tuner_dummy.c
@@ -0,0 +1,76 @@
+//
+// Created by Sascha on 6/27/22.
+//
+
+#include <stdio.h>
+#include "at_coll_tuner.h"
+
+void AT_coll_tune_init(void) {
+  printf("WARNING: dummy init... use LD_PRELOAD\n");
+}
+
+void AT_coll_tune_finalize(void) {
+  printf("WARNING: dummy finalize... use LD_PRELOAD\n");
+}
+
+int AT_get_allreduce_selection_id(int buf_size, int comm_size) {
+  // empty
+  return 0;
+}
+
+int AT_get_barrier_selection_id(int buf_size, int comm_size) {
+  // empty
+  return 0;
+}
+
+int AT_get_bcast_selection_id(int buf_size, int comm_size) {
+  // empty
+  return 0;
+}
+
+int AT_get_allgather_selection_id(int buf_size, int comm_size) {
+  // empty
+  return 0;
+}
+
+int AT_get_alltoall_selection_id(int buf_size, int comm_size) {
+  // empty
+  return 0;
+}
+
+int AT_get_allreduce_ompi_id(int our_alg_id) {
+  // empty
+  return 0;
+}
+
+int AT_get_allreduce_ompi_segsize(int our_alg_id) {
+  // empty
+  return 64;
+}
+
+void AT_enable_collective_sampling(int flag) {
+  // empty
+}
+
+int AT_is_collective_sampling_enabled(void) {
+  // empty
+  return 0;
+}
+
+int AT_is_collective_sampling_possible(void) {
+  // empty
+  return 0;
+}
+
+double AT_get_time(void) {
+  // empty
+  return 0.0;
+}
+
+void AT_record_start_timestamp(const AT_mpi_call_t callid, const int our_alg_id, const int buf_size, const int comm_size) {
+  // empty
+}
+
+void AT_record_end_timestamp(const AT_mpi_call_t callid) {
+  // empty
+}
